27b7fd7f765da5b9ad5e3dfee5a1bec7e825d3b9
salman hadi <salmanhadi1993@gmail.com>
2018-10-13 13:35:24 +0000

    Ckeditor installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   root to: "posts#index"
 
   resources :posts
