4f827f602de4979c1206753c38fcd342b6dde455
salman hadi <salmanhadi1993@gmail.com>
2018-10-13 13:32:49 +0000

    CRUD functions without ckeditor

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+Rails.application.routes.draw do
+  root to: "posts#index"
 
+  resources :posts
+end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
